7406573d99349e89e1bbfc2c734d92c8ff440fc6
alexander <catalyst0955@gmail.com>
2018-12-15 16:08:48 +0800

    pageNotFound change to correct name, and use wildcard to redirect invalid route to not-found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
-import { PageNotFountComponent } from './page-not-fount/page-not-fount.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -24,7 +24,8 @@ const appRoutes: Routes = [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]},
-
+  {path: 'not-found', component: PageNotFoundComponent },
+  {path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
@@ -36,7 +37,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFountComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/page-not-found/page-not-found.component.spec.ts
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PageNotFountComponent } from './page-not-fount.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
-describe('PageNotFountComponent', () => {
-  let component: PageNotFountComponent;
-  let fixture: ComponentFixture<PageNotFountComponent>;
+describe('PageNotFoundComponent', () => {
+  let component: PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageNotFountComponent ]
+      declarations: [ PageNotFoundComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PageNotFountComponent);
+    fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
